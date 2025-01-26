537c54d3eec4223ee3007a8e8adeb70adbcb7d95
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-26 15:48:35 +0530

    Update [...auth0].js

--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -1,5 +1,36 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+// import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+
+// export default handleAuth({
+//     signup: handleLogin({ authorizationParams: { screen_hint: "signup" } }),
+// });
+
+// pages/api/auth/[...nextauth].js
+import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
 
 export default handleAuth({
-    signup: handleLogin({ authorizationParams: { screen_hint: "signup" } }),
+    login: handleLogin({
+        authorizationParams: {
+            screen_hint: "signup",
+        },
+        returnTo: "/profile",
+    }),
+
+    signup: handleLogin({
+        authorizationParams: {
+            screen_hint: "signup",
+        },
+        returnTo: "/profile",
+    }),
+
+    logout: handleLogout({ returnTo: "/" }), // Redirect to home after logout
+
+    onError(req, error) {
+        console.error(error);
+        return {
+            redirect: {
+                destination: '/error', // Redirect to an error page
+                permanent: false,
+            },
+        };
+    },
 });
