2330d94641980cdb75607f1293d61abae1ab413a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-30 10:17:53 +0530

    Update [[...chatId]].js

--- src/pages/chat/[[...chatId]].js
+++ src/pages/chat/[[...chatId]].js
@@ -1,8 +1,19 @@
+import Head from "next/head";
 
 export default function Chat() {
   return (
-    <div>
-      <p>Welcome to the Chat ID Page</p>
-    </div>
+    <>
+      <Head>
+        <title>New Chat</title>
+      </Head>
+      <div className="grid h-screen grid-cols-[260px_1fr]">
+        <div className="bg-gray-800">Sidebar</div>
+        <div className=" flex flex-col bg-gray-700">
+          <div className="bg-gray-600 text-white">Header</div>
+          <div className="flex-1 bg-gray-500">Messages</div>
+          <footer className="bg-gray-800 p-10">Footer</footer>
+        </div>
+      </div>
+    </>
   );
 }
